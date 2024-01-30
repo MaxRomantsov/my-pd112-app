752276e87ce496a139ad92b17b77aa9c94c445b3
MaxRomantsov <Roma_ua72@student.itstep.org>
2024-01-30 14:56:03 +0200

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import DefaultLayout from "./components/containers/default/DefaultLayout.tsx";
+import {Route, Routes} from "react-router-dom";
+import HomePage from "./components/home/HomePage.tsx";
+
+const App: React.FC = () => {
+
+    return (
+        <>
+            <Routes>
+                <Route path="/" element={<DefaultLayout />}>
+                    <Route index element={<HomePage />} />
+                </Route>
+            </Routes>
+        </>
+    );
+};
+
+export default App;
