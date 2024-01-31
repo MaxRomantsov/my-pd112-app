be2fa0783d5d09891973c1f03bf586a855318ab1
MaxRomantsov <Roma_ua72@student.itstep.org>
2024-01-31 12:43:00 +0200

    add category with image

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import DefaultLayout from "./components/containers/default/DefaultLayout.tsx";
 import {Route, Routes} from "react-router-dom";
 import HomePage from "./components/home/HomePage.tsx";
+import CategoryCreatePage from "./components/categories/create/CategoryCreatePage.tsx";
 
 const App: React.FC = () => {
 
@@ -10,6 +11,7 @@ const App: React.FC = () => {
             <Routes>
                 <Route path="/" element={<DefaultLayout />}>
                     <Route index element={<HomePage />} />
+                    <Route path={"categories/create"} element={<CategoryCreatePage />} />
                 </Route>
             </Routes>
         </>
